0ea1578a33e1312403886cdb771980cc19ae63c9
Ayushsotiya <ayushsotiya56@gmail.com>
2025-06-14 12:27:48 +0530

    fixing the app.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import NavBar from './components/common/NavBar';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import ForgotPassword from './pages/ForgotPassword';
